b6a67dbe52c39adb01e6fdd116d676c3b2d3de50
Adam Binnersley <AdamB7586@users.noreply.github.com>
2017-07-07 14:17:34 +0100

    Remove duplicate $

--- src/Caching/RedisCache.php
+++ src/Caching/RedisCache.php
@@ -12,7 +12,7 @@ class RedisCache implements CacheInterface{
         if(!extension_loaded('redis')){
             die('Redis extension is not loaded');
         }
-        $$this->cache = new Redis();
+        $this->cache = new Redis();
     }
     
     /**
