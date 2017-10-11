13c066a66d4925aa559e693ecea13bca0cdd855c
Adam Binnersley <abinnersley@gmail.com>
2017-10-11 11:55:37 +0100

    Update regular expression

--- src/Modifiers/SafeString.php
+++ src/Modifiers/SafeString.php
@@ -10,6 +10,6 @@ class SafeString{
      * @return string Will return the safe value
      */
     public static function makeSafe($string){
-        return preg_replace("/^[a-z0-9_]+$/i", "", $string);
+        return preg_replace("/[^A-Za-z0-9_]/", "", $string);
     }
 }
