d179084626f75dac597c4fb9f2397b543845738e
Adam Binnersley <abinnersley@gmail.com>
2017-10-11 11:31:57 +0100

    Add additional protection against MySQL injections

---
+++ src/Modifiers/String.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace DBAL\Modifiers;
+
+class String{
+    
+    /**
+     * Make any SQL field names safe
+     * @param string $string The string that you wish to make safe
+     * @return string Will return the safe value
+     */
+    public static function makeSafe($string){
+        return preg_replace("/^[a-z0-9_]+$/i", "", $string);
+    }
+}
